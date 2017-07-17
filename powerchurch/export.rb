9b55f2103ffb86f9ce7aa3fee35a2432025fc173
Tim Morgan <tim@timmorgan.org>
2017-07-16 23:20:59 -0500

    Add all dates and include status

--- powerchurch/export.rb
+++ powerchurch/export.rb
@@ -1,16 +1,43 @@
+# list statuses here to define a person's status in OneBody
+# any status not listed will cause a person to be 'inactive' in OneBody
+
+STATUSES = {
+  pending: [
+    'Visitor'
+  ],
+  active: [
+    'Member - Active',
+    'Non-Member - Active'
+  ]
+}.freeze
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 require 'bundler'
 Bundler.require
 
 require 'csv'
 
-COLUMNS = %w(
+COLUMNS = %w[
   legacy_id
   first_name
   middle_name
   last_name
-  birthday
   email
   gender
+  birthday
+  date2
+  date3
+  date4
+  date5
+  date6
+  date7
+  date8
+  date9
+  date10
+  date11
+  date12
+  status
   family_legacy_id
   family_name
   family_last_name
@@ -20,7 +47,7 @@ COLUMNS = %w(
   family_state
   family_zip
   family_country
-).freeze
+].freeze
 
 (DATA_PATH, OUT_PATH) = ARGV
 
@@ -31,8 +58,27 @@ end
 
 me = DBF::Table.new(File.join(DATA_PATH, 'me.dbf'))
 ma = DBF::Table.new(File.join(DATA_PATH, 'ma.dbf'))
+me_codes = DBF::Table.new(File.join(DATA_PATH, 'macodes.dbf'))
+
+CODES = me_codes.each_with_object({}) do |code, hash|
+  hash[code['FIELD']] ||= {}
+  hash[code['FIELD']][code['CODE']] = code['DESCRIPT']
+end
 
 families = ma.each_with_object({}) { |f, h| h[f['MAIL_NO']] = f }
+
+def format_date(date)
+  date && date.strftime('%Y-%m-%d')
+end
+
+def status(code)
+  description = CODES.fetch('CATEGORY', {})[code]
+  if (found = STATUSES.detect { |_, matching| matching.include?(description) })
+    found.first.to_s
+  else
+    'inactive'
+  end
+end
 
 CSV.open(OUT_PATH, 'w') do |csv|
   csv << COLUMNS
@@ -44,9 +90,21 @@ CSV.open(OUT_PATH, 'w') do |csv|
       person['FIRSTNAME'].strip,
       person['MIDDLENAME'].strip,
       person['LASTNAME'].strip,
-      person['BORN'] && person['BORN'].strftime('%Y-%m-%d'),
       person['E_MAIL'].strip,
       person['M_F'].strip,
+      format_date(person['BORN']),
+      format_date(person['DATE2']),
+      format_date(person['DATE3']),
+      format_date(person['DATE4']),
+      format_date(person['DATE5']),
+      format_date(person['DATE6']),
+      format_date(person['DATE7']),
+      format_date(person['DATE8']),
+      format_date(person['DATE9']),
+      format_date(person['DATE10']),
+      format_date(person['DATE11']),
+      format_date(person['DATE12']),
+      status(person['STATUS']),
       person['MAIL_NO'],
       family['NAMELINE'].strip,
       family['LASTNAME'].strip,
